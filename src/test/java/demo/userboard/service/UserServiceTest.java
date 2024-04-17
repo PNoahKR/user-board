e2e12f4b3f0d417489cbbebd1da1a696b206bd41
noah <qkrwldn0818@naver.com>
2024-04-17 20:32:37 +0900

    TEST: 엔티티인지오류

--- src/test/java/demo/userboard/service/UserServiceTest.java
+++ src/test/java/demo/userboard/service/UserServiceTest.java
@@ -3,24 +3,25 @@ package demo.userboard.service;
 
 import demo.userboard.domain.Gender;
 import demo.userboard.domain.User;
-import demo.userboard.repository.MemoryUserRepository;
 import demo.userboard.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@Transactional
 class UserServiceTest {
 
-    UserService userService;
-    UserRepository userRepository;
 
-    @BeforeEach
-    public void beforeEach() {
-        userRepository = new MemoryUserRepository();
-        userService = new UserServiceImpl(userRepository);
-    }
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
 
     @Test
     void join() {
@@ -38,6 +39,9 @@ class UserServiceTest {
     @Test
     void findByEmail() {
         User user = new User();
+        user.setName("hi");
+        user.setNickname("no");
+        user.setPassword("12");
         user.setEmail("asd@asd.com");
         userRepository.save(user);
 
