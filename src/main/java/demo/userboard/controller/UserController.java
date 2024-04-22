3ff21a6ee0aab010501db92a8e0355962836b4c2
noah <qkrwldn0818@naver.com>
2024-04-22 11:29:18 +0900

    fix: 어노테이션 제거

--- src/main/java/demo/userboard/controller/UserController.java
+++ src/main/java/demo/userboard/controller/UserController.java
@@ -1,21 +1,15 @@
 package demo.userboard.controller;
 
-import demo.userboard.domain.User;
 import demo.userboard.dto.request.JoinRequestDto;
 import demo.userboard.dto.response.JoinResponseDto;
 import demo.userboard.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
 @RequiredArgsConstructor
 public class UserController {
 
