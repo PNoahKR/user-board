74f398f7286ab0a8caea852fdeef8b94f2695d88
noah <qkrwldn0818@naver.com>
2024-04-26 13:46:02 +0900

    fix:CommonResponse 생성자 추가, TestController 삭제

--- src/main/java/demo/userboard/global/common/response/CommonResponse.java
+++ src/main/java/demo/userboard/global/common/response/CommonResponse.java
@@ -9,6 +9,12 @@ public class CommonResponse<T> {
     private final String message;
     private final T data;
 
+    public CommonResponse(Integer status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
     public CommonResponse(Integer status, String message) {
         this.status = status;
         this.message = message;
