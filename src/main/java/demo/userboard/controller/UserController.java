ec89b734f642559e7299622912483113ceebe933
noah <qkrwldn0818@naver.com>
2024-04-30 13:56:36 +0900

    fix:로그인 검증 로직 삭제

--- src/main/java/demo/userboard/controller/UserController.java
+++ src/main/java/demo/userboard/controller/UserController.java
@@ -4,8 +4,8 @@ import demo.userboard.dto.request.JoinRequestDto;
 import demo.userboard.dto.request.LoginRequestDto;
 import demo.userboard.dto.response.JoinResponseDto;
 import demo.userboard.dto.response.LoginResponseDto;
-import demo.userboard.global.common.response.CustomErrorCode;
-import demo.userboard.global.core.exception.CustomException;
+import demo.userboard.global.common.response.CommonResponse;
+import demo.userboard.global.common.util.ApiResponseUtil;
 import demo.userboard.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -26,12 +26,8 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> loginUser(@RequestBody LoginRequestDto loginRequestDto) {
-        LoginResponseDto responseDto = userService.login(loginRequestDto);
-        if (responseDto == null) {
-            throw new CustomException(CustomErrorCode.UNAUTHORIZED);
-        } else {
-            return ResponseEntity.ok(responseDto);
-        }
+    public CommonResponse<LoginResponseDto> loginUser(@RequestBody LoginRequestDto loginRequestDto) {
+
+        return ApiResponseUtil.success(userService.login(loginRequestDto));
     }
 }
