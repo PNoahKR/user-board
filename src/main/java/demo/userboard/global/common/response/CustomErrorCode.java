e712160283d3151cee3a2c093e1f9b3776fd2b53
noah <qkrwldn0818@naver.com>
2024-05-02 19:53:46 +0900

    fix: 오류코드 네이밍 수정

--- src/main/java/demo/userboard/global/common/response/CustomErrorCode.java
+++ src/main/java/demo/userboard/global/common/response/CustomErrorCode.java
@@ -12,7 +12,7 @@ public enum CustomErrorCode {
     SUCCESS(OK, "success"),
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "server error"),
     // 사용자 정의 코드
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "로그인 실패"),
+    LOGIN_FAIL(HttpStatus.BAD_REQUEST, "로그인 실패"),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증 실패");
 
     private final HttpStatus status;
