a1565e4f3eb3a6f1038d5bebe770888d5085ae76
noah <qkrwldn0818@naver.com>
2024-05-09 17:46:24 +0900

    Feat: JpaMemoryBoardRepository 레퍼지토리 생성

---
+++ src/main/java/demo/userboard/repository/JpaMemoryBoardRepository.java
@@ -0,0 +1,27 @@
+package demo.userboard.repository;
+
+import demo.userboard.domain.Board;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JpaMemoryBoardRepository implements BoardRepository {
+
+    private final EntityManager em;
+
+    @Override
+    public Board save(Board board) {
+        em.persist(board);
+        return board;
+    }
+
+    @Override
+    public Optional<Board> findBoardById(Long boardId) {
+        Board board = em.find(Board.class, boardId);
+        return Optional.ofNullable(board);
+    }
+}
