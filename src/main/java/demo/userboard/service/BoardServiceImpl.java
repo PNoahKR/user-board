539151da3b5aeb00d858bd3ae0fb5d7da1b950ef
noah <qkrwldn0818@naver.com>
2024-05-09 17:46:56 +0900

    Feat: BoardService 서비스 클래스 생성

---
+++ src/main/java/demo/userboard/service/BoardServiceImpl.java
@@ -0,0 +1,31 @@
+package demo.userboard.service;
+
+import demo.userboard.domain.Board;
+import demo.userboard.domain.User;
+import demo.userboard.dto.request.PostRequestDto;
+import demo.userboard.global.common.response.CustomErrorCode;
+import demo.userboard.global.core.exception.CustomException;
+import demo.userboard.repository.BoardRepository;
+import demo.userboard.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+    private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public Long post(Long userId, PostRequestDto requestDto) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(CustomErrorCode.NOT_FOUND_LOGIN_INFO));
+        Board board = requestDto.toEntity(user);
+        Board saveBoard = boardRepository.save(board);
+        return saveBoard.getId();
+    }
+}
