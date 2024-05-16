5b2320774faf496bd0e827e2434b63148a6fc06c
noah <qkrwldn0818@naver.com>
2024-05-16 13:39:11 +0900

    fix: 생성자 초기화 제거

--- src/main/java/demo/userboard/domain/PasswordConverter.java
+++ src/main/java/demo/userboard/domain/PasswordConverter.java
@@ -2,13 +2,14 @@ package demo.userboard.domain;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Converter
 public class PasswordConverter implements AttributeConverter<String, String> {
 
-    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public String convertToDatabaseColumn(String attribute) {
